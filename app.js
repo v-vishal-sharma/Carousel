6d5ab34a65d3aa5550586e8e8b8775561d13c07e
Vishal Sharma <vishals200415@gmail.com>
2022-09-05 22:29:25 +0530

    buttons hide

--- app.js
+++ app.js
@@ -6,27 +6,55 @@ slides.forEach(function(slide,index){
     slide.style.left = `${index * 100}%`;
 });
 
+// infinite scroll
+// let counter = 0;
+// nextBtn.addEventListener('click',function(){
+//     counter++;
+//     if(counter > slides.length-1) {
+//         counter = 0;
+//         carousel();
+//     }else{
+//         carousel();
+//     }
+// })
+// prevBtn.addEventListener('click',function(){
+//     counter--;
+//     if(counter < 0) {
+//         counter = slides.length - 1;
+//         carousel();
+//     }else{
+//         carousel();
+//     }
+// })
+
+// function carousel(){
+//     slides.forEach(function(slide){
+//         slide.style.transform = `translateX(-${counter*100}%)`
+//     })
+// }
+
+// button hide
 let counter = 0;
 nextBtn.addEventListener('click',function(){
     counter++;
-    if(counter > slides.length-1) {
-        counter = 0;
-        carousel();
-    }else{
-        carousel();
-    }
-})
-prevBtn.addEventListener('click',function(){
-    counter--;
-    if(counter < 0) {
-        counter = slides.length - 1;
-        carousel();
-    }else{
-        carousel();
-    }
+    carousel();
 })
 
+prevBtn.addEventListener("click", function(){
+    counter--;
+    carousel();
+})
 function carousel(){
+    if (counter < slides.length-1) {
+        nextBtn.style.display = "block";
+    }else{
+        nextBtn.style.display = "none";
+    }
+    if(counter > 0){
+        prevBtn.style.display = "block";
+    }else{
+        prevBtn.style.display = "none";
+    }
     slides.forEach(function(slide){
         slide.style.transform = `translateX(-${counter*100}%)`
     })
